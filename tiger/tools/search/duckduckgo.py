2dec4937523cff88f276c06e8dc12c58bee1d1ab
Restyled.io <commits@restyled.io>
2024-04-08 19:59:12 +0000

    Restyled by whitespace

--- tiger/tools/search/duckduckgo.py
+++ tiger/tools/search/duckduckgo.py
@@ -4,7 +4,7 @@ from duckduckgo_search import DDGS
 def search(query: str, max_number: int = 20) -> list:
     """
 
-    :param query: str: 
+    :param query: str:
     :param max_number: int:  (Default value = 20)
 
     """
