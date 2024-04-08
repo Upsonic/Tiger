1c8cb05c50fdcfc82448e20e17aa2eb4ab07f268
Restyled.io <commits@restyled.io>
2024-04-08 19:59:07 +0000

    Restyled by pyment

--- tiger/tools/search/duckduckgo.py
+++ tiger/tools/search/duckduckgo.py
@@ -2,6 +2,12 @@ from duckduckgo_search import DDGS
 
 
 def search(query: str, max_number: int = 20) -> list:
+    """
+
+    :param query: str: 
+    :param max_number: int:  (Default value = 20)
+
+    """
     return [result["href"] for result in DDGS().text(query, max_results=max_number)]
 
 
