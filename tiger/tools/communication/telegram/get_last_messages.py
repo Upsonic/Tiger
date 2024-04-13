6ef22bf748b46fd21590fb2c450e635fbbf31488
Restyled.io <commits@restyled.io>
2024-04-13 18:08:16 +0000

    Restyled by black

--- tiger/tools/communication/telegram/get_last_messages.py
+++ tiger/tools/communication/telegram/get_last_messages.py
@@ -17,21 +17,18 @@ def get_last_messages(number: str, limit=100):
 
     async def get_messages(num, limit):
         async with TelegramClient(
-                "upsonic_tiger", 21659296,
-                "7d0ebd20538d88ab0629eb926acb08f7") as client:
+            "upsonic_tiger", 21659296, "7d0ebd20538d88ab0629eb926acb08f7"
+        ) as client:
             messages = await client.get_messages(num, limit=limit)
             the_messages_list = {}
             for each_ms in messages:
                 the_messages_list[each_ms.id] = {
-                    "id":
-                    each_ms.id,
-                    "message":
-                    each_ms.text,
-                    "date":
-                    each_ms.date,
-                    "sender":
-                    (await
-                     client.get_entity(each_ms.peer_id.user_id)).username,
+                    "id": each_ms.id,
+                    "message": each_ms.text,
+                    "date": each_ms.date,
+                    "sender": (
+                        await client.get_entity(each_ms.peer_id.user_id)
+                    ).username,
                 }
             return the_messages_list
 
--- tiger/tools/communication/telegram/send_message.py
+++ tiger/tools/communication/telegram/send_message.py
@@ -17,8 +17,8 @@ def send_message(number: str, message: str) -> str:
 
     async def send_message(number, message):
         async with TelegramClient(
-                "upsonic_tiger", 21659296,
-                "7d0ebd20538d88ab0629eb926acb08f7") as client:
+            "upsonic_tiger", 21659296, "7d0ebd20538d88ab0629eb926acb08f7"
+        ) as client:
             return (await client.send_message(number, message)).id
 
     result = asyncio.run(send_message(number, message))
