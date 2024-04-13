e3ea55a931ed6016ae0c47bbf8fa97988cee4b7a
Onur Atakan ULUSOY <atadogan06@gmail.com>
2024-04-13 21:01:11 +0300

    Added telegram module

---
+++ tiger/tools/communication/telegram/delete_message.py
@@ -0,0 +1,18 @@
+from telethon.sync import TelegramClient
+import time
+import asyncio
+
+
+def delete_message(number:str, message:str):
+    import nest_asyncio
+    nest_asyncio.apply()
+
+    async def del_message(num, message):
+        async with TelegramClient("upsonic_tiger", 21659296, '7d0ebd20538d88ab0629eb926acb08f7') as client:
+            await client.delete_messages(num, message)
+
+    asyncio.run(del_message(number, message))
+
+tool_name = "communication.telegram.delete_message"
+tool_obj = delete_message
+tool_requirements = ["telethon==1.34.0"]
---
+++ tiger/tools/communication/telegram/send_message.py
@@ -0,0 +1,19 @@
+from telethon.sync import TelegramClient
+import time
+import asyncio
+
+
+def send_message(number: str, message: str) -> str:
+    import nest_asyncio
+    nest_asyncio.apply()
+
+    async def send_message(number, message):
+        async with TelegramClient("upsonic_tiger", 21659296, '7d0ebd20538d88ab0629eb926acb08f7') as client:
+            return (await client.send_message(number, message)).id
+
+    result = asyncio.run(send_message(number, message))
+    return result
+
+tool_name = "communication.telegram.send_message"
+tool_obj = send_message
+tool_requirements = ["telethon==1.34.0"]
