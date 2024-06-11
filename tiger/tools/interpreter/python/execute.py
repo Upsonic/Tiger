b0ef2f771e961fa199d2bd017b067c2c95e9c03c
Onur ULUSOY <atadogan06@gmail.com>
2024-06-11 14:31:53 +0300

    Added try except to execute

--- tiger/tools/interpreter/python/execute.py
+++ tiger/tools/interpreter/python/execute.py
@@ -1,16 +1,20 @@
 
 
 def execute(cell:str) -> str:
-    from IPython import get_ipython
+    try:
+        from IPython import get_ipython
 
-    ipython = get_ipython()
-    result = ipython.run_cell(cell)
-    log = str(result.result)
-    if result.error_before_exec is not None:
-        log += f"\n{result.error_before_exec}"
-    if result.error_in_exec is not None:
-        log += f"\n{result.error_in_exec}"
-    return log
+        ipython = get_ipython()
+        result = ipython.run_cell(cell)
+        log = str(result.result)
+        if result.error_before_exec is not None:
+            log += f"\n{result.error_before_exec}"
+        if result.error_in_exec is not None:
+            log += f"\n{result.error_in_exec}"
+        return log
+    except Exception as e:
+        return e
+        
 
 tool_name = "interpreter.python.execute"
 tool_obj = execute
